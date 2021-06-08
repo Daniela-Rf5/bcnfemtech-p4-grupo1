878f5f80985a1e68ab021e15d20886d0b63c4a06
yassscoder <yasminlcg@gmail.com>
2021-06-08 18:45:21 +0200

    Creating gamesservice

---
+++ src/main/java/com/legacygames/legacygames/controllers/GamesController.java
@@ -0,0 +1,30 @@
+package com.legacygames.legacygames.controllers;
+
+import com.legacygames.legacygames.Services.GamesService;
+import com.legacygames.legacygames.models.Games;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class GamesController {
+    private GamesService gamesService;
+
+    @Autowired
+    public GamesController(GamesService gamesService) {
+        this.gamesService = gamesService;
+    }
+
+    @GetMapping("/games")
+    String listGames(Model model) {
+        List<Games> games = gamesService.allGames();
+        model.addAttribute("title", "Games list");
+        model.addAttribute("games", games);
+        return "games/all";
+    }
+    }
+
+
