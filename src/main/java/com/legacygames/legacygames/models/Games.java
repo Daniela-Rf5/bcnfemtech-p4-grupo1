da581ba2756dbd96cdfe444226328500665af85a
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-06-08 12:58:09 +0200

    creando models y constructor

---
+++ src/main/java/com/legacygames/legacygames/models/Games.java
@@ -0,0 +1,110 @@
+package com.legacygames.legacygames.models;
+
+import org.springframework.data.annotation.Id;
+import javax.persistence.*;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name="games")
+
+public class Games implements Serializable{
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long id;
+    private String title;
+    private String platform;
+    private Integer year;
+    private Float originalPrice;
+    private String tag;
+    private Float discount;
+    private Float applyDiscount;
+    private String category;
+    private String publisher;
+    private Long pegi;
+    private String pegiDescriptor;
+
+    public Long getId() {return id;}
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getTitle() {return title;}
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getPlatform(){return platform;}
+    public void setPlatform(String platform){
+        this.platform=platform;
+    }
+
+    public Integer getYear(){return year;}
+    public void setYear(Integer year){
+        this.year=year;
+    }
+
+    public Float getOriginalPrice(){return originalPrice;}
+    public void setOriginalPrice(Float originalPrice){
+        this.originalPrice=originalPrice;
+    }
+
+    public String getTag(){return tag;}
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Float getDiscount(){return discount;}
+        public void setDiscount(Float discount){
+        this.discount=discount;
+        }
+    public Float getApplyDiscount(){return applyDiscount;}
+    public void setApplyDiscount(Float applyDiscount){
+        this.applyDiscount=applyDiscount;
+    }
+    public String getCategory(){return category;}
+    public void setCategory(String category){
+        this.category=category;
+    }
+
+    public String getPublisher(){return publisher;}
+    public void setPublisher(String publisher){
+        this.publisher=publisher;
+    }
+    public Long getPegi() {return pegi;}
+    public void setPegi(Long pegi){
+        this.pegi=pegi;
+    }
+
+    public String getPegiDescriptor(){return pegiDescriptor;}
+    public void setPegiDescriptor(String pegiDescriptor) {
+        this.pegiDescriptor = pegiDescriptor;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
