1924c8668fd6180a3165a274ebf6dc0ea1f3ecf0
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-06-08 13:57:53 +0200

    añadiendo override

--- src/main/java/com/legacygames/legacygames/models/Games.java
+++ src/main/java/com/legacygames/legacygames/models/Games.java
@@ -24,61 +24,118 @@ public class Games implements Serializable {
     private Long pegi;
     private String pegiDescriptor;
 
-    public Long getId() {return id;}
-    public void setId(Long id){
-        this.id=id;
+    public Long getId() {
+        return id;
     }
 
-    public String getTitle() {return title;}
-    public void setTitle(String title){
-        this.title=title;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getPlatform(){return platform;}
-    public void setPlatform(String platform){
-        this.platform=platform;
+    public String getTitle() {
+        return title;
     }
 
-    public Integer getYear(){return year;}
-    public void setYear(Integer year){
-        this.year=year;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public Float getOriginalPrice(){return originalPrice;}
-    public void setOriginalPrice(Float originalPrice){
-        this.originalPrice=originalPrice;
+    public String getPlatform() {
+        return platform;
     }
 
-    public String getTag(){return tag;}
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Float getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(Float originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
     public void setTag(String tag) {
         this.tag = tag;
     }
 
-    public Float getDiscount(){return discount;}
-        public void setDiscount(Float discount){
-        this.discount=discount;
-        }
-    public Float getApplyDiscount(){return applyDiscount;}
-    public void setApplyDiscount(Float applyDiscount){
-        this.applyDiscount=applyDiscount;
-    }
-    public String getCategory(){return category;}
-    public void setCategory(String category){
-        this.category=category;
+    public Float getDiscount() {
+        return discount;
     }
 
-    public String getPublisher(){return publisher;}
-    public void setPublisher(String publisher){
-        this.publisher=publisher;
-    }
-    public Long getPegi() {return pegi;}
-    public void setPegi(Long pegi){
-        this.pegi=pegi;
+    public void setDiscount(Float discount) {
+        this.discount = discount;
     }
 
-    public String getPegiDescriptor(){return pegiDescriptor;}
+    public Float getApplyDiscount() {
+        return applyDiscount;
+    }
+
+    public void setApplyDiscount(Float applyDiscount) {
+        this.applyDiscount = applyDiscount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Long getPegi() {
+        return pegi;
+    }
+
+    public void setPegi(Long pegi) {
+        this.pegi = pegi;
+    }
+
+    public String getPegiDescriptor() {
+        return pegiDescriptor;
+    }
+
     public void setPegiDescriptor(String pegiDescriptor) {
         this.pegiDescriptor = pegiDescriptor;
+    }
+
+    @Override
+    public String toString() {
+        return "Games{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", platform='" + platform + '\'' +
+                ", year=" + year +
+                ", originalPrice=" + originalPrice +
+                ", tag='" + tag + '\'' +
+                ", discount=" + discount +
+                ", applyDiscount=" + applyDiscount +
+                ", category='" + category + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", pegi=" + pegi +
+                ", pegiDescriptor='" + pegiDescriptor + '\'' +
+                '}';
     }
 }
 
