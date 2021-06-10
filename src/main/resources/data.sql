c576183a1509d75f637e38b8c0b99a864b7b91ce
yassscoder <yasminlcg@gmail.com>
2021-06-10 14:22:48 +0200

    añadiendo 1 juego

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 
 INSERT INTO games(title, platform, yearR, price, tag, discount, discounted, category, publisher, pegi, descripter)
-VALUES('Wii Sports','Wii', '2006', '19.99',' ',' 1','18.99 ','Sports','Nintendo', '7', ' '),    --,)' '),--
-       ('Super Mario Bros.','NES', '1985', '14.99',' ',' 1','2 ','Sports','Nintendo','7', ' ');    --,' ');
+VALUES('Wii Sports','Wii', '2006', '19.99',' ',' 10','18.99 ','Sports','Nintendo', '7', ' '),
+       ('Call of duty:Black Ops II', 'X360', '2012', '29.99','MEGA','20', '23.99', 'Shooter', 'Activision', '16', 'Realistic violence'),
+       ('Super Mario Bros.','NES', '1985', '14.99',' ',' 10','13.5 ','Sports','Nintendo','7', ' ');
