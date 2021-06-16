e2f7467cc8551be88236758e4fc64228bc927b48
yassscoder <yasminlcg@gmail.com>
2021-06-16 11:34:44 +0200

    changing home controller

--- src/main/java/com/legacygames/legacygames/controllers/HomeController.java
+++ src/main/java/com/legacygames/legacygames/controllers/HomeController.java
@@ -1,14 +1,29 @@
 package com.legacygames.legacygames.controllers;
 
+import com.legacygames.legacygames.Services.GamesService;
+import com.legacygames.legacygames.models.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private GamesService gamesService;
+
+    @Autowired
+    public HomeController(GamesService gamesService) {
+        this.gamesService = gamesService;
+    }
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    String listGames(Model model) {
+        List<Game> games = gamesService.allGames();
+        model.addAttribute("title", "Games list");
+        model.addAttribute("games", games);
+
+        return "games/inventory";
     }
 }
-
