b99c09483778a3b7e84213911b7e447f7c0d006d
yassscoder <yasminlcg@gmail.com>
2021-06-18 11:10:25 +0200

    cambios postmapping, formulario

--- src/main/java/com/legacygames/legacygames/controllers/GamesController.java
+++ src/main/java/com/legacygames/legacygames/controllers/GamesController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,7 +22,7 @@ public class GamesController {
 
 
     @GetMapping("/games/new")
-    String newGame(Model model){
+    String newGame(Model model) {
         Game game = new Game();
         model.addAttribute("game", game);
         model.addAttribute("title", "create new game");
@@ -28,6 +30,12 @@ public class GamesController {
 
     }
 
+    @PostMapping("/games/new")
+    public String addGame(@ModelAttribute Game game) {
+        gamesService.save(game);
+       // return "redirect:/index";
+        return "redirect:/games";
+        //return "redirect:home";
     }
-
+}
 
