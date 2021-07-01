351076804019c5d3b29f9ecc98d79a27badc16e6
Marialauramn16 <hikco25@gmail.com>
2021-07-01 12:04:02 +0200

    ajustando platform list

--- src/main/java/com/legacygames/legacygames/controllers/GamesController.java
+++ src/main/java/com/legacygames/legacygames/controllers/GamesController.java
@@ -54,7 +54,6 @@ public class GamesController {
         model.addAttribute("pegis", pegiService.allPegis()); // se utiliza para que se muestre la lista desplegable y hay que agregar un PegiService
         model.addAttribute("categories", categoryService.allCategories()); // se utiliza para que se muestre la lista desplegable y hay que agregar un CategoryService
         model.addAttribute("platforms", platformService.allPlatform()); // se utiliza para que se muestre la lista desplegable y hay que agregar un CategoryService
-
         return "games/newgames";
     }
 
